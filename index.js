7792c63fded9962e9a00d7a61eabbbc790d383ae
Marco Comes <marco.comes@studenti.unimi.it>
2016-05-05 13:06:40 +0200

    Implement test for render a single template

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 'use strict';
 
+var dust = require('dustjs-linkedin');
+var fs = require('fs');
+
+dust.onLoad = function(template, callback) {
+    fs.readFile(template, 'utf8', function(err, data) {
+        if (err) return callback(err);
+
+        callback(null, data);
+    });
+};
+
 module.exports = {
     engine: function() {
         return function(template, options, callback) {
-            callback(null, null);
+            dust.render(template, options, function(err, output) {
+                callback(err, output);
+            });
         };
     }
 };
